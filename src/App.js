c88c20efa781c6d5ca8321d3f976584653513b51
Sruthi <30sruthihariharan2002@gmail.com>
2025-04-29 12:35:58 +0530

    real-estate-ai-code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
+import './styles/variable.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import HomePage from './Components/Home';
+import Header from "./Navbar/Navbar";
+import AboutUs from "./About/About";
+import Services from "./Service/Service";
+import Interpretingservice from "./Service1/Interpretingservice";
+import Relocationservice from "./Service2/Relocationservice";
+import Contact from "./Contact/Contact";
+
 
 function App() {
+
+// for some effectss
+    useEffect(() => {
+        AOS.init({
+          duration: 1000, 
+          once: false,     
+          mirror: true  
+        });
+    
+        const handleScroll = () => AOS.refresh();
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/interpreting-service" element={<Interpretingservice />} />
+        <Route path="/relocation-service" element={<Relocationservice />} />
+        <Route path="/contact" element={<Contact />} />
+
+
+      </Routes>
+    </Router>
   );
 }
 
